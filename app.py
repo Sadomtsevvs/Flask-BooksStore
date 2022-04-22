a761c5daf38dc33fb4257e287a1436c0aaa165c9
Sadomtsevvs <67393853+Sadomtsevvs@users.noreply.github.com>
2022-04-22 22:18:05 +0300

    Second

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
+@app.route('/profile/<username>')
+def profile(username: str):
+    return render_template('profile.html', username=username.capitalize(), isActive=True)
 
 
 # @app.route('/admin')
